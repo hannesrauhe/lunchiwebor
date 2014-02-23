c52159b835e397dc53d27b514282dfdeb286f2f7
Hannes Rauhe <hannes@scitivity.net>
2014-02-23 21:12:56 +0100

    *fix for sending (buffer was too large) - not sure about utf-8 for now

--- lunchinator.js
+++ lunchinator.js
@@ -171,7 +171,7 @@ function ab2str(buf) {
 }
 
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var buf = new ArrayBuffer(str.length); // 2 bytes for each char
   var bufView = new Uint8Array(buf);
   for (var i=0, strLen=str.length; i<strLen; i++) {
     bufView[i] = str.charCodeAt(i);
